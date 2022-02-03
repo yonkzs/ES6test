fc82035d4471ce451b1090106452ea4807631e7d
Nao Suzuki <39011805+yonkzs@users.noreply.github.com>
2022-02-03 14:19:53 +0900

    2/3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import "./styles.css";
+// const vall = 1 > 0 ? "trueです" : "falseです";
+// console.log(vall);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// const num = "1300";
+// // console.log(num.toLocaleString());
+
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+// console.log(formattedNum);
+
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "100を超えています！" : "許容範囲内です";
+};
+console.log(checkSum(50, 70));
